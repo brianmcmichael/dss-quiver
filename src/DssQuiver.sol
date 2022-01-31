efdbef8b57055a2114ead90574aa8942f97b20a0
Brian McMichael <brian@brianmcmichael.com>
2022-01-31 13:28:26 -0500

    Mint new NFT

--- src/DssQuiver.sol
+++ src/DssQuiver.sol
@@ -1,5 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.6;
 
-contract DssQuiver {
+import "ds-deed.git/deed.sol";
+
+contract DssQuiver is DSDeed("DSSQuiver", "DSSQ") {
+    constructor() {
+
+    }
+
+    function make() external {
+        uint256 _id = mint(msg.sender);
+
+    }
 }
