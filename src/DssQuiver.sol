2f82db096d7e68e4b06225fa98d6b126af8f3ddb
Brian McMichael <brian@brianmcmichael.com>
2022-01-31 15:56:25 -0500

    interim changes

--- src/DssQuiver.sol
+++ src/DssQuiver.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.6;
 
-import "ds-deed.git/deed.sol";
+import "ds-deed/deed.sol";
 
 contract DssQuiver is DSDeed("DSSQuiver", "DSSQ") {
     constructor() {
 
     }
 
-    function make() external {
+    function make() external returns (uint256 id, address proxy) {
+        address proxy;
         uint256 _id = mint(msg.sender);
-
     }
 }
