07eb45983678d100b21ccc99758ed04e242ccc90
Brian McMichael <brian@brianmcmichael.com>
2022-02-01 09:11:23 -0500

    map address to id

--- src/DssQuiver.sol
+++ src/DssQuiver.sol
@@ -6,11 +6,16 @@ import "./DssProxy.sol";
 
 // NFT Proxy manager
 contract DssQuiver is DSDeed("DSSQuiver", "DSSQ") {
+
+    mapping (uint256 => address) proxies;
+
     constructor() {
     }
 
     function make() external returns (uint256 id, address proxy) {
+        // TODO precalculate address with create2 and use it as the NFT label
         uint256 id = _mint(msg.sender, "");
         address proxy = address(new DssProxy(address(this), id));
+        proxies[id] = proxy;
     }
 }
